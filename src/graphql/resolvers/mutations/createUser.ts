bc093d5aadc0764ad17d744bf7db1be37963745a
Lars Severson <larsseverson1@gmail.com>
2025-01-26 19:56:27 -0600

    [UPDATE]: Adjusting how users are stored and the resolvers that use user data

--- src/graphql/resolvers/mutations/createUser.ts
+++ src/graphql/resolvers/mutations/createUser.ts
@@ -1,24 +1,28 @@
-import { Context, RDSRequest, util } from '@aws-appsync/utils'
-import { createPgStatement, toJsonObject, insert } from '@aws-appsync/utils/rds'
+import { AppSyncIdentityCognito, Context, RDSRequest, util } from '@aws-appsync/utils'
+import { createPgStatement, toJsonObject, insert, sql } from '@aws-appsync/utils/rds'
+import { User } from '@src/graphql/types/userTypes'
 
 interface CreateUserArgs {
- username: string
  cognitoId: string
+ email: string
 }
 
 export const request = (ctx: Context): RDSRequest | null => {
-  const { username, cognitoId }: CreateUserArgs = ctx.args
+  const { cognitoId, email }: CreateUserArgs = ctx.args
 
-  const query = insert({
-    table: 'users_view',
-    values: { username, cognitoId },
-    returning: '*'
-  })
+  const query = sql`
+    INSERT INTO users_view ("cognitoId", email)
+    VALUES (${cognitoId}, ${email})
+    ON CONFLICT (email)
+    DO UPDATE 
+      SET email = EXCLUDED.email
+    RETURNING *
+  `
 
   return createPgStatement(query)
 }
 
-export const response = (ctx: Context): any => {
+export const response = (ctx: Context): Record<'user', User> | void => {
   const { error, result } = ctx
 
   if (error) {
@@ -30,6 +34,21 @@ export const response = (ctx: Context): any => {
   }
 
   const user = toJsonObject(result)[0][0]
+
+  if (user.user) {
+    const identity = ctx.identity as AppSyncIdentityCognito
+
+    const srcCogId = identity.sub || undefined
+    const resCogId = user.user.cognitoId || undefined
+
+    if (srcCogId !== resCogId) {
+      return util.appendError(
+        'You are not authorized to get this users info',
+        '403',
+        { user: null }
+      )
+    }
+  }
 
   return user
 }
--- src/graphql/resolvers/queries/user/user.ts
+++ src/graphql/resolvers/queries/user/user.ts
@@ -1,18 +1,22 @@
-import { Context, RDSRequest, util, runtime } from '@aws-appsync/utils'
+import { Context, RDSRequest, util, runtime, AppSyncIdentityIAM, AppSyncIdentityCognito } from '@aws-appsync/utils'
 import { createPgStatement, toJsonObject, select } from '@aws-appsync/utils/rds'
 import { User } from '@src/graphql/types/userTypes'
 import { graphqlFields } from '@src/graphql/utils/graphqlFields'
 
 interface UserArgs {
-  id: number
+  id?: number | undefined
+
+  cognitoId?: string | undefined
 }
 
 export const request = (ctx: Context): RDSRequest | null => {
+  const { id, cognitoId }: UserArgs = ctx.args
+
+  if (!id && !cognitoId) return runtime.earlyReturn(null)
+
   const fieldName = ctx.info.fieldName
   const fields = graphqlFields(ctx.info.selectionSetList, fieldName)
   const columns = fields[fieldName]
-
-  const { id }: UserArgs = ctx.args
 
   if (!fields || columns.length === 0) {
     return runtime.earlyReturn({ id })
@@ -21,13 +25,18 @@ export const request = (ctx: Context): RDSRequest | null => {
   const query = select<number>({
     from: 'users_view',
     columns,
-    where: { id: { eq: id } }
+    where: {
+      or: [
+        id ? { id: { eq: id } } : {},
+        cognitoId ? { cognitoId: { eq: cognitoId } } : {}
+      ]
+    }
   })
 
   return createPgStatement(query)
 }
 
-export const response = (ctx: Context): any => {
+export const response = (ctx: Context): Record<string, User | null> | void => {
   const { error, result } = ctx
 
   if (error) {
@@ -38,7 +47,22 @@ export const response = (ctx: Context): any => {
     )
   }
 
-  const user = toJsonObject(result)[0][0]
+  const user: Record<'user', User> = toJsonObject(result)[0][0]
+
+  if (user.user) {
+    const identity = ctx.identity as AppSyncIdentityCognito
+
+    const srcCogId = identity.sub || undefined
+    const resCogId = user.user.cognitoId || undefined
+
+    if (srcCogId !== resCogId) {
+      return util.appendError(
+        'You are not authorized to get this users info',
+        '403',
+        { user: null }
+      )
+    }
+  }
 
   return user
 }
