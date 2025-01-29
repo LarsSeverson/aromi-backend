fb1085026b9e5fc16c60a6ad4fc9bb7c0d9b1f3d
Lars Severson <larsseverson1@gmail.com>
2025-01-28 21:05:24 -0600

    [UPDATE]: Fixed logic where the fragrance accord does not exist to begin with in the voteOnAccord mutation

--- src/graphql/resolvers/mutations/voteOnAccord.ts
+++ src/graphql/resolvers/mutations/voteOnAccord.ts
@@ -2,21 +2,40 @@ import { Context, RDSRequest, util } from '@aws-appsync/utils'
 import { createPgStatement, toJsonObject, insert, sql, select } from '@aws-appsync/utils/rds'
 
 interface VoteOnAccordArgs {
- fragranceAccordId: number
+ fragranceId: number
+ accordId: number
  userId: number
 }
 
 export const request = (ctx: Context): RDSRequest | null => {
-  const { fragranceAccordId, userId }: VoteOnAccordArgs = ctx.args
+  const { fragranceId, accordId, userId }: VoteOnAccordArgs = ctx.args
 
   const query = sql`
+    WITH fragrance_accord_cte AS (
+    INSERT INTO fragrance_accords (fragrance_id, accord_id)
+    SELECT ${fragranceId}, ${accordId} 
+    WHERE NOT EXISTS (
+        SELECT 1 FROM fragrance_accords 
+        WHERE fragrance_id = ${fragranceId} 
+          AND accord_id = ${accordId}
+    )
+    RETURNING id
+    ), fragrance_accord_id AS (
+        SELECT id FROM fragrance_accords 
+        WHERE fragrance_id = ${fragranceId}
+          AND accord_id = ${accordId}
+        UNION ALL
+        SELECT id FROM fragrance_accord_cte
+    )
     INSERT INTO fragrance_accord_votes_view ("fragranceAccordId", "userId")
-    VALUES (${fragranceAccordId}, ${userId})
+    SELECT id, ${userId} FROM fragrance_accord_id
     ON CONFLICT ("fragranceAccordId", "userId")
-    DO UPDATE SET "deletedAt" = CASE
-      WHEN fragrance_accord_votes_view."deletedAt" IS NULL THEN CURRENT_TIMESTAMP
-      ELSE NULL
-    END
+    DO UPDATE SET "deletedAt" = CASE 
+      WHEN fragrance_accord_votes_view."deletedAt" IS NULL 
+        THEN CURRENT_TIMESTAMP 
+        ELSE NULL 
+      END,
+        "updatedAt" = CURRENT_TIMESTAMP
     RETURNING *
   `
 
