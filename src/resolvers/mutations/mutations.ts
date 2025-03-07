a6e362791a7712148039e42a4666042b84005677
Lars Severson <larsseverson1@gmail.com>
2025-03-06 18:34:49 -0600

    Added cursor based pagination for user collections. Other resolvers that use limit and offset will be converted to cursor based. Also added create createCollection and addFraganceToCollection mutations

--- src/resolvers/mutations/mutations.ts
+++ src/resolvers/mutations/mutations.ts
@@ -2,11 +2,11 @@ import { voteOnFragrance } from './fragrance/vote-on-fragrance'
 import { voteOnAccord } from './fragrance/vote-on-accord'
 import { voteOnNote } from './fragrance/vote-on-note'
 import { voteOnTrait } from './fragrance/vote-on-trait'
-import { upsertUser } from './user/upsertUser'
+import { upsertUser } from './user/upsert-user'
 import { reviewFragrance } from './fragrance/review-fragrance'
 import { voteOnReview } from './fragrance/vote-on-review'
 import { type MutationResolvers } from '@src/generated/gql-types'
-import { createCollection } from './user/createCollection'
+import { createCollection } from './user/create-collection'
 import { addFragranceToCollection } from './fragrance/add-fragrance-to-collection'
 
 export const Mutation: MutationResolvers = {
