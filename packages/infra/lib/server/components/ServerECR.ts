18948afe1505a1ee677f629b0c0f113b11783f97
Edward Dillenger <larsseverson1@gmail.com>
2025-12-08 19:59:10 -0600

    Trigger server image CI

--- packages/infra/lib/server/components/ServerECR.ts
+++ packages/infra/lib/server/components/ServerECR.ts
@@ -1,10 +1,14 @@
 import { Repository } from 'aws-cdk-lib/aws-ecr'
 import type { ServerECRComponentProps } from '../types.js'
 import { ServerConfig } from './ServerConfig.js'
+import { CfnOutput } from 'aws-cdk-lib'
 
 export class ServerECRComponent {
   readonly repositoryId: string
   readonly repository: Repository
+
+  readonly outputRepositoryUriId: string
+  readonly outputRepositoryUri: CfnOutput
 
   constructor (props: ServerECRComponentProps) {
     const { stack } = props
@@ -16,6 +20,12 @@ export class ServerECRComponent {
       imageScanOnPush: ServerConfig.ECR_CONFIG.imageScanOnPush,
       imageTagMutability: ServerConfig.ECR_CONFIG.imageTagMutability,
       lifecycleRules: ServerConfig.ECR_CONFIG.liceCycleRules
+    })
+
+    this.outputRepositoryUriId = `${this.repositoryId}-uri`
+    this.outputRepositoryUri = new CfnOutput(stack, this.outputRepositoryUriId, {
+      value: this.repository.repositoryUri,
+      exportName: this.outputRepositoryUriId
     })
   }
 }
