529a47f7555028336911b9d566c7922d3c20e386
Edward Dillenger <larsseverson1@gmail.com>
2025-12-10 21:08:30 -0600

    Added socket debugging

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -69,6 +69,12 @@ export const startServer = async (): Promise<string> => {
 
   await server.start()
 
+  app.use((req, res, next) => {
+    console.log(`[REQ] ${req.method} ${req.url}`)
+    res.on('close', () => { console.log(`[RES CLOSE] ${req.method} ${req.url}`) })
+    next()
+  })
+
   app.get('/health', (_req, res) => {
     res.status(200).send('ok')
   })
