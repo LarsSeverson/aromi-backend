6d3c862e9fd04754ca007b8441c6d396ec3111f8
LarsSeverson <larsseverson1@gmail.com>
2026-01-29 18:23:07 -0600

    Change to db types

--- packages/shared/src/db/sql/types.sql
+++ packages/shared/src/db/sql/types.sql
@@ -2,44 +2,44 @@ CREATE TYPE public.asset_status AS enum('staged', 'ready');
 
 CREATE TYPE public.avatar_status AS enum('PENDING', 'PROCESSING', 'READY', 'FAILED');
 
-CREATE TYPE public.edit_status AS enum('APPROVED', 'REJECTED', 'PENDING');
+CREATE TYPE public.edit_status AS enum('PENDING', 'APPROVED', 'REJECTED');
 
-CREATE TYPE public.edit_type AS enum('accord', 'note', 'fragrance', 'brand');
+CREATE TYPE public.edit_type AS enum('brand', 'fragrance', 'note', 'accord');
 
 CREATE TYPE public.fragrance_concentration AS enum(
-  'PARFUM',
+  'BODY_MIST',
+  'EAU_FRAICHE',
+  'EDC',
   'EDP',
   'EDT',
   'OIL',
   'OTHER',
-  'BODY_MIST',
-  'EAU_FRAICHE',
-  'EDC'
+  'PARFUM'
 );
 
-CREATE TYPE public.fragrance_reaction AS enum('dislike', 'like');
+CREATE TYPE public.fragrance_reaction AS enum('like', 'dislike');
 
-CREATE TYPE public.fragrance_status AS enum('REFORMULATED', 'DISCONTINUED', 'CURRENT');
+CREATE TYPE public.fragrance_status AS enum('CURRENT', 'DISCONTINUED', 'REFORMULATED');
 
 CREATE TYPE public.fragrance_trait_enum AS enum(
-  'complexity',
+  'longevity',
   'sillage',
+  'complexity',
   'balance',
   'allure',
-  'gender',
-  'longevity'
+  'gender'
 );
 
-CREATE TYPE public.job_status AS enum('PROCESSING', 'FAILED', 'QUEUED', 'SUCCESS');
+CREATE TYPE public.job_status AS enum('QUEUED', 'PROCESSING', 'SUCCESS', 'FAILED');
 
-CREATE TYPE public.note_layer_enum AS enum('base', 'middle', 'top');
+CREATE TYPE public.note_layer_enum AS enum('top', 'middle', 'base');
 
-CREATE TYPE public.post_type AS enum('FRAGRANCE', 'TEXT', 'MEDIA');
+CREATE TYPE public.post_type AS enum('TEXT', 'MEDIA', 'FRAGRANCE');
 
-CREATE TYPE public.request_status AS enum('PENDING', 'DENIED', 'ACCEPTED', 'DRAFT');
+CREATE TYPE public.request_status AS enum('DRAFT', 'PENDING', 'ACCEPTED', 'DENIED');
 
-CREATE TYPE public.request_type AS enum('note', 'fragrance', 'brand', 'accord');
+CREATE TYPE public.request_type AS enum('fragrance', 'brand', 'accord', 'note');
 
-CREATE TYPE public.upload_status AS enum('uploaded', 'failed', 'pending');
+CREATE TYPE public.upload_status AS enum('pending', 'uploaded', 'failed');
 
-CREATE TYPE public.user_role AS enum('MODERATOR', 'ADMIN', 'USER');
+CREATE TYPE public.user_role AS enum('USER', 'MODERATOR', 'ADMIN');
